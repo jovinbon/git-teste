18e17033b84d7c9f796d5e6e48cc8d40b78ccf08
Cristiano Fontes <jovinbon@hotmail.com>
2020-09-04 23:53:56 -0300

    terceiro commit

--- ClasseA.java
+++ ClasseA.java
@@ -1,3 +1,11 @@
 public class ClasseA{
     private String nome;
+
+    public String getNome(){
+        return this.nome;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
 }
--- ClasseB.java
+++ ClasseB.java
@@ -1,3 +1,11 @@
 public class ClasseB{
     private String idade;
+
+    public String getIdade(){
+        return this.idade;
+    }
+
+    public void setIdade(String idade){
+        this.idade = idade;
+    }
 }
